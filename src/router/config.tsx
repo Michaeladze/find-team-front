6592d664641f72d9959ae73c3ee76252b534d387
michaeladze <michael.kutateladze@yandex.ru>
2020-11-10 22:07:56 +0300

    INIT

---
+++ src/router/config.tsx
@@ -0,0 +1,22 @@
+import React, { ComponentType, lazy, LazyExoticComponent, ReactNode } from 'react';
+
+/** Везде, где подключается <Router routes={routes}/>, кроме App.tsx, routes: IRoute[] берется из пропсов IProps. */
+
+export interface IRoute {
+  /** Адрес */
+  path: string;
+  /** Точность совпадения */
+  exact: boolean;
+  /** Защищенный роут */
+  private?: boolean;
+  /** Компонент */
+  component?: LazyExoticComponent<ComponentType<any>>;
+  /** Дочерние роуты */
+  routes?: IRoute[];
+  /** Редирект*/
+  redirect?: string;
+  /** Прелоудер ф*/
+  fallback: NonNullable<ReactNode> | null;
+}
+
+export const routes = [];
